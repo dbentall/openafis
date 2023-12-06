db6d3a9d3335c6fe3d7ad7b5bd8411aad07a8a09
Daniel Bentall <daniel.bentall@plantandfood.co.nz>
2023-12-07 11:00:05 +1300

    Make more pythonic

--- src/openafis/openafis.pyx
+++ src/openafis/openafis.pyx
@@ -14,11 +14,17 @@ cdef class PyTemplateISO19794_2_2005:
     def __dealloc__(self):
         del self.c_instance
 
-    def from_file(self, path: str):
-        return self.c_instance.load(path.encode())
+    @staticmethod
+    def from_file(path: str, id: int = 0):
+        py_template = PyTemplateISO19794_2_2005(id)
+        py_template.c_instance.load(path.encode())
+        return py_template
 
-    def from_bytes(self, data: bytes):
-        return self.c_instance.load(data, len(data))
+    @staticmethod
+    def from_bytes(data: bytes, id: int = 0):
+        py_template = PyTemplateISO19794_2_2005(id)
+        py_template.c_instance.load(data, len(data))
+        return py_template
 
 
 cdef class PyMatchSimilarity:
@@ -27,26 +33,26 @@ cdef class PyMatchSimilarity:
 
     def __cinit__(
         self,
-        MaximumLocalDistance = None,
-        MaximumGlobalDistance = None,
-        MinimumMinutiae = None,
-        MaximumRotations = None,
-        MaximumAngleDifference = None,
-        MaximumDirectionDifference = None,
+        max_local_dist = None,
+        max_global_dist = None,
+        min_minutiae = None,
+        max_rotations = None,
+        max_angle_diff = None,
+        max_direction_diff = None,
     ):
         self.c_instance = new MatchSimilarity()
-        if MaximumLocalDistance is not None:
-            self.param.MaximumLocalDistance = MaximumLocalDistance
-        if MaximumGlobalDistance is not None:
-            self.param.MaximumGlobalDistance = MaximumGlobalDistance
-        if MinimumMinutiae is not None:
-            self.param.MinimumMinutiae = MinimumMinutiae
-        if MaximumRotations is not None:
-            self.param.MaximumRotations = MaximumRotations
-        if MaximumAngleDifference is not None:
-            self.param.MaximumAngleDifference = MaximumAngleDifference
-        if MaximumDirectionDifference is not None:
-            self.param.MaximumDirectionDifference = MaximumDirectionDifference
+        if max_local_dist is not None:
+            self.param.MaximumLocalDistance = max_local_dist
+        if max_global_dist is not None:
+            self.param.MaximumGlobalDistance = max_global_dist
+        if min_minutiae is not None:
+            self.param.MinimumMinutiae = min_minutiae
+        if max_rotations is not None:
+            self.param.MaximumRotations = max_rotations
+        if max_angle_diff is not None:
+            self.param.MaximumAngleDifference = max_angle_diff
+        if max_direction_diff is not None:
+            self.param.MaximumDirectionDifference = max_direction_diff
 
     def __dealloc__(self):
         del self.c_instance
